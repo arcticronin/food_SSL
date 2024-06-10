fe4351314cb2a21c9321ed1df931640db776c3b8
clod2000 <clacolturi@gmail.com>
2024-06-10 20:49:14 +0200

    "added custom net and stormfire"

--- scripts/models.py
+++ scripts/models.py
@@ -189,3 +189,108 @@ class Decoder(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.decode(x)
+
+
+
+
+
+
+############ Custom Model ############
+class StormFire(nn.Module):
+    # This model is based on Fire module from SqueezeNet with the addition of BatchNorm 
+    # and the change of ReLU to LeakyReLU
+
+    # autoencoder
+    def __init__(
+        self,
+        inplanes: int,
+        squeeze_planes: int,
+        expand1x1_planes: int,
+        expand3x3_planes: int,
+        
+    ) -> None:
+        super().__init__()
+        self.inplanes = inplanes
+
+        self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
+        self.squeeze_activation = nn.LeakyReLU(inplace=True)
+        self.squeeze_bn = nn.BatchNorm2d(squeeze_planes)
+       
+        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes, kernel_size=1)
+        self.expand1x1_activation = nn.LeakyReLU(inplace=True)
+        self.expand1x1_bn = nn.BatchNorm2d(expand1x1_planes)
+        
+        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes, kernel_size=3, padding=1)
+        self.expand3x3_activation = nn.LeakyReLU(inplace=True)
+        self.expand3x3_bn = nn.BatchNorm2d(expand3x3_planes)
+
+    def forward(self, x):
+        x = self.squeeze_bn(self.squeeze_activation(self.squeeze(x)))
+        return torch.cat([
+            self.expand1x1_bn(
+                self.expand1x1_activation(self.expand1x1(x))
+            ),
+            self.expand3x3_bn(
+                self.expand3x3_activation(self.expand3x3(x)) 
+            ),
+            ], 1)
+            
+        
+
+
+class StormSqueezeNet(nn.Module):
+    # This net is a custom version of SqueezeNet: we replaced the Fire module with our custom StormFire module,
+    # modified the final convolutional layer into a fully connected layer
+    # we used leaky ReLU instead of ReLU
+    # we added BatchNorm after each convolutional layer
+    # we modified the number of fire modules and the number of filters in each fire module
+
+    def __init__(self, num_classes: int = 251, dropout: float = 0.5) -> None:
+        super().__init__()
+        self.num_classes = num_classes
+        self.features = nn.Sequential(
+            
+            nn.Conv2d(3, 64, kernel_size=3, stride=2),
+            nn.LeakyReLU(inplace=True),
+
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+
+            StormFire(64, 16, 64, 64),
+            StormFire(128, 16, 64, 64),
+
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+
+            StormFire(128, 32, 128, 128),
+            StormFire(256, 32, 128, 128),
+
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+
+            StormFire(256, 48, 192, 192),
+            StormFire(384, 64, 256, 256),
+            
+        )
+
+        # Final convolution is initialized differently from the rest
+        final_conv = nn.Conv2d(512, self.num_classes, kernel_size=1)
+        
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            final_conv,
+            nn.LeakyReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1)),
+        )
+
+        self.classifier2 = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(512, 512),
+            nn.LeakyReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(512, self.num_classes),
+        )
+       
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.classifier2(x)
+        return torch.flatten(x, 1)
+
